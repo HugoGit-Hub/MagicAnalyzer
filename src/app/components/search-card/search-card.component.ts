9dabe50bcf304947f411d9d348b35fb1ec3ba4fa
Hugo <hugo.decuq@septeo.com>
2024-10-07 11:57:53 +0200

    Create search bar

--- src/app/components/search-card/search-card.component.ts
+++ src/app/components/search-card/search-card.component.ts
@@ -1,34 +1,46 @@
 import {Component} from '@angular/core';
-import {NgForOf, NgIf} from '@angular/common';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
 import {SearchCardExternal} from '../../externals/search-card.external';
+import {debounceTime, distinctUntilChanged, Observable, of, Subject, switchMap} from 'rxjs';
 
 @Component({
   selector: 'search-cards',
   standalone: true,
   imports: [
     NgIf,
-    NgForOf
+    NgForOf,
+    AsyncPipe
   ],
   templateUrl: './search-card.component.html',
   styleUrl: 'search-card.component.css'
 })
 
 export class SearchCard {
-  protected filteredResults:string[] = [];
+  private searchQuery: Subject<string> = new Subject<string>();
+
+  protected filteredResults:Observable<string[]> = of([]);
 
   constructor(private searchCardExternal: SearchCardExternal) {
+    this.filteredResults = this.searchQuery.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(query => {
+        if (query.trim().length === 0) {
+          return of([]);
+        }
+
+        return this.searchCardExternal.autocomplete(query);
+      })
+    );
+
+    this.filteredResults.subscribe(results => {
+      console.log('Filtered Results:', results);
+    });
   }
 
+  protected onSearch(event: Event): void {
+    const query: string = (event.target as HTMLInputElement).value.toLowerCase().trim();
 
-  onSearch(event: Event): void {
-    const query = (event.target as HTMLInputElement).value.toLowerCase();
-
-    if (query.trim().length > 0) {
-      this.searchCardExternal.autocomplete(query).subscribe(results => {
-        this.filteredResults = results;
-      });
-    } else {
-      this.filteredResults = [];
-    }
+    this.searchQuery.next(query);
   }
 }
--- src/app/externals/search-card.external.ts
+++ src/app/externals/search-card.external.ts
@@ -1,5 +1,5 @@
 import {HttpClient} from '@angular/common/http';
-import {map, Observable} from 'rxjs';
+import {map, Observable, of, tap} from 'rxjs';
 import {Catalog} from '../dto/Catalog';
 import {Injectable} from '@angular/core';
 
@@ -7,12 +7,18 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class SearchCardExternal {
+  private cache: { [key: string]: string[] } = {};
+
   constructor(private http: HttpClient) { }
 
   public autocomplete(value: string): Observable<string[]> {
-    return this.http.get<Catalog>("https://api.scryfall.com/cards/autocomplete?q=" + value)
-      .pipe(
-        map(response => response.data)
-      );
+    if (this.cache[value]) {
+      return of(this.cache[value]);
+    }
+
+    return this.http.get<Catalog>(`https://api.scryfall.com/cards/autocomplete?q=${value}`).pipe(
+      tap(response => this.cache[value] = response.data),
+      map(response => response.data)
+    );
   }
 }
