35472507c3afe8334a078d3990f9761cd7565dd5
hugo <hugo.decuq.34@icloud.com>
2024-10-13 00:48:02 +0200

    Create deck feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {AnalyzerFeature} from './features/analyzer/analyzer.feature';
 import {SearchFeature} from './features/search/search.feature';
 import {HomeFeature} from './features/home/home.feature';
+import {DeckFeature} from './features/deck/deck.feature';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeFeature },
   { path: 'analyzer', component: AnalyzerFeature },
-  { path: 'search', component: SearchFeature }
+  { path: 'search', component: SearchFeature },
+  { path: 'deck', component: DeckFeature },
 ];
---
+++ src/app/features/deck/deck.feature.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'deck',
+  standalone: true,
+  templateUrl: './deck.feature.html'
+})
+
+export class DeckFeature {
+
+}
